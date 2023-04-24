91ff63619becaa2e658ed01c5951105a57641439
Ervin Tuzlic <ervin.tuzlic@longrun.solutions>
2023-04-24 20:38:33 +0200

    Add project files.

---
+++ TestButton/Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazorBootstrap;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TestButton.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorBootstrap();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
